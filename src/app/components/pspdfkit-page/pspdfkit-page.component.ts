6be1162b6243ecd88f337607151de9410f0f5c03
Patrick Pale <patrick.pale@atomicobject.com>
2022-06-27 11:18:05 -0400

    Assigned instance to a field.

--- src/app/components/pspdfkit-page/pspdfkit-page.component.ts
+++ src/app/components/pspdfkit-page/pspdfkit-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import PSPDFKit from 'pspdfkit';
+import PSPDFKit, { Instance } from 'pspdfkit';
 
 @Component({
   selector: 'app-pspdfkit-page',
@@ -7,19 +7,20 @@ import PSPDFKit from 'pspdfkit';
   styleUrls: ['./pspdfkit-page.component.scss']
 })
 export class PspdfkitPageComponent {
-  title = "PSPDFKit for Web Angular Example";
+  instance: Instance | undefined;
 
   ngAfterViewInit() {
     PSPDFKit.load({
       // Use the assets directory URL as a base URL. PSPDFKit will download its library assets from here.
-      baseUrl: location.protocol + "//" + location.host + "/assets/",
-      document: "/assets/cc219b.pdf",
-      container: "#pspdfkit-container",
+      baseUrl: location.protocol + '//' + location.host + '/assets/',
+      document: '/assets/cc219b.pdf',
+      container: '#pspdfkit-container',
     }).then(instance => {
       // For the sake of this demo, store the PSPDFKit for Web instance
       // on the global object so that you can open the dev tools and
       // play with the PSPDFKit API.
       (window as any).instance = instance;
+      this.instance = instance;
     });
   }
 }
